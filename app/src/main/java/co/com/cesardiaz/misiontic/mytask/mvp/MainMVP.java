15b158bc0eef1153b8cef7c6b55cf38a0de595d8
ruben dario conde carvajal <condehalo117@hotmail.com>
2021-12-19 13:49:23 -0500

    carpetas e interfaces

---
+++ app/src/main/java/co/com/cesardiaz/misiontic/mytask/mvp/MainMVP.java
@@ -0,0 +1,15 @@
+package co.com.cesardiaz.misiontic.mytask.mvp;
+
+public interface MainMVP {
+
+    interface Model{
+
+    }
+
+    interface Presenter {
+
+    }
+
+    interface View {
+    }
+}
